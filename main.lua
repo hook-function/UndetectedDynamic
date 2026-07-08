d3b366766405e9709ed020ad202a66dba630055f
rennielowia-cpu <rennielowia@gmail.com>
2026-07-08 18:51:57 +0300

    e

--- main.lua
+++ main.lua
@@ -1,15 +1,21 @@
-local id = game.PlaceId
-local url = "https://raw.githubusercontent.com/hook-function/UndetectedDynamic/main/games/"..id..".lua"
-
 local ok, ct = pcall(function()
     return game:HttpGet(url)
 end)
 
-pcall(function()
+local loadOk, execErr = pcall(function()
     loadstring(ct)()
+end)
+
+if loadOk then
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Undetected Dynamic",
         Text = "Loader successful, we are so UD",
         Duration = 5,
     })
-end)
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Error",
+        Text = tostring(execErr),
+        Duration = 5,
+    })
+end
