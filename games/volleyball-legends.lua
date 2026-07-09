2bc26f79bf4273a7b2d93116c65734761df85e42
rennielowia-cpu <rennielowia@gmail.com>
2026-07-09 14:46:49 +0300

    e

---
+++ games/volleyball-legends.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e9f6b2fc712d3f0911901fec6896dd8488d5c14c45ae84df680e82f7f0b56c30/download"))()
--- main.lua
+++
@@ -1,9 +0,0 @@
-local id = game.PlaceId
-local games = {
-    [2753915549]= "https://raw.githubusercontent.com/deividcomsono/UndetectedDynamic/main/games/bloxfruits.lua",
-}
-if table.find(games, id) then
-    loadstring(game:HttpGet(games[id]))()
-else
-    print("Game not supported")
-end
